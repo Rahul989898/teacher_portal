a65f596de91dd311d776ab1ec5a2c929d31eac76
rahul ahirwar <Rahul2.Ahirwar@ril.com>
2024-10-23 11:45:57 +0530

    Add some changes related to UI

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -33,13 +33,26 @@ class StudentsController < ApplicationController
   end
 
   # PATCH/PUT /students/:id
-  def update
+ def update
+  @student = Student.find(params[:id])
   if @student.update(student_params)
-    redirect_to root_path, notice: 'Student was successfully updated.'
+    flash[:notice] = 'Student was successfully updated.'
+    respond_to do |format|
+      format.js  # This will look for update.js.erb
+      format.html { redirect_to root_path }  # Fallback for regular HTML requests
+    end
   else
-    render :edit
+    flash.now[:alert] = 'Error updating student. Please check the input.'
+    respond_to do |format|
+      format.js { render 'edit' }  # Render edit template for AJAX
+      format.html { render :edit }  # Fallback for regular HTML requests
+    end
   end
 end
+
+
+
+
 
   # DELETE /students/:id
   def destroy
